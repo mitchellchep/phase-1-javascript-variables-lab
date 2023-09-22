ef279890f6a61a056896203d740656441a2ce7d4
mitchellkoech <mitchikoechi3.7@gmail.com>
2023-09-22 21:21:46 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+function calculateprofit(mostProfitableNeighborhood) {
+    return chelsea;
+}
+
+function getcompanyCeo(){
+    return "Susan Smith";
+}
+
